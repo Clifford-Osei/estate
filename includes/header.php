d1e3d96165f09374f158b96757017c0ec83acd67
Clifford <oseitutuclifford21@gmail.com>
2023-09-22 13:31:09 +0000

    the hero section

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md ">
+	<div class="container-fluid">
+
+		<a href="index.php"><img class="navbar-brand" src="../assets/images/logo.png" height="80"></img></a>
+
+		 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      		<span class="navbar-toggler-icon"></span>
+    	</button>
+
+    	<div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<ul class="navbar-nav ms-auto me-auto">
+				<li class="nav-item h4"><a class="nav-link text-white" href="index.php">Home</a></li>
+				<li class="nav-item h4"><a class="nav-link text-white" href="properties.php">Properties</a></li>
+				<li class="nav-item h4"><a class="nav-link text-white" href="about.php">About Us</a></li>
+				<li class="nav-item h4"><a class="nav-link text-white" href="contact.php">Contact</a></li>
+			</ul>
+		<h4 class="navbar-text" style="color:white"><i class="fa fa-phone"></i> Web Dev Consult</h4>
+	</div>
+</div>
+	
+</nav> 
